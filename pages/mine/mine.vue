e1f7671e75cf42aceed7f952479f96653a6a34a8
AaronChu <571388081@qq.com>
2021-10-12 18:25:49 +0800

    feat: 赞赏弹出框

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2021-09-20 17:58:56
  * @LastEditors: AaronChu
- * @LastEditTime: 2021-10-11 17:25:49
+ * @LastEditTime: 2021-10-12 18:24:09
 -->
 <template>
   <view class="page">
@@ -27,15 +27,22 @@
             <u-cell-item icon="list-dot" title="遗物部件默认折叠" :arrow="false">
               <u-switch slot="right-icon" v-model="collapse" @change="setCollapse"></u-switch>
             </u-cell-item>
-            <u-cell-item icon="chat" title="问题反馈"  @click="navPage(0)"></u-cell-item>
-            <u-cell-item icon="thumb-up" title="鸣谢名单"  @click="navPage(1)"></u-cell-item>
-            <u-cell-item icon="clock" title="计划日志"  @click="navPage(3)"></u-cell-item>
-            <u-cell-item icon="info-circle" title="关于"  @click="navPage(2)"></u-cell-item>
-            <u-cell-item icon="gift" title="赞赏支持"  @click="navPage(4)"></u-cell-item>
+            <u-cell-item icon="chat" title="问题反馈" @click="navPage(0)"></u-cell-item>
+            <u-cell-item icon="thumb-up" title="鸣谢名单" @click="navPage(1)"></u-cell-item>
+            <u-cell-item icon="clock" title="计划日志" @click="navPage(3)"></u-cell-item>
+            <u-cell-item icon="info-circle" title="关于" @click="navPage(2)"></u-cell-item>
+            <u-cell-item icon="gift" title="赞赏支持" @click="$refs.popup.open()"></u-cell-item>
           </u-cell-group>
         </view>
       </view>
     </scroll-view>
+    <uni-popup ref="popup" type="center">
+      <view class="support">
+        <image src="/static/support.jpg" mode="widthFix" show-menu-by-longpress />
+        <view class="tips">小程序不支持长按识别二维码，需保存到本地才能扫码</view>
+        <view class="tips">要是能来帮我打下 钢铁蝠力使 就更好了</view>
+      </view>
+    </uni-popup>
   </view>
 </template>
 
@@ -45,44 +52,41 @@ export default {
   data() {
     return {
       collapse: false,
-      count:{
+      count: {
         remain: 0,
-        part: 0
+        part: 0,
       },
-      navList: [
-        '/pages/suggest/suggest',
-        '/pages/acknowledgement/acknowledgement',
-        '/pages/about/about',
-        '/pages/logs/logs',
-        '/pages/support/support',
-      ]
+      navList: ['/pages/suggest/suggest', '/pages/acknowledgement/acknowledgement', '/pages/about/about', '/pages/logs/logs'],
     }
   },
   async onLoad() {
     const res = await index()
     this.count = res
   },
-  methods:{
+  methods: {
     /**
      * @description: 设置折叠面板默认显示
      */
-    setCollapse(e){
+    setCollapse(e) {
       uni.setStorageSync('collapse', !this.collapse)
     },
     /**
      * @description: 页面跳转通用函数
      * @param {Number} index 点击的列表下标
      */
-    navPage(index){
+    navPage(index) {
       uni.navigateTo({ url: this.navList[index] })
-    }
-  }
+    },
+    showSupport() {
+      this.$refs.popup.open()
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .header {
-  background-image: url('/static/mine_bg.jpg');
+  background-image: url('/static/mine_bg.jpeg');
   background-repeat: no-repeat;
   background-size: 100% 100%;
   height: 442rpx;
@@ -111,6 +115,18 @@ export default {
     border-radius: 16rpx;
     background: #fff;
     overflow: hidden;
+  }
+}
+.support {
+  width: 680rpx;
+  margin: 0 auto;
+  background: #fff;
+  border-radius: 16rpx;
+  text-align: center;
+  padding: 16rpx;
+  .tips {
+    font-size: 24rpx;
+    margin: 16rpx 0;
   }
 }
 </style>
